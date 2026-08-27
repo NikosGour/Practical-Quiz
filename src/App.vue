9a20c1b28bf7aabbd93b09702375e9269ae294e6
NikosGour <nikosgournak@gmail.com>
2026-08-27 18:48:10 +0300

    add question text

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,42 @@ import { Button } from "primevue";
 
 interface Question {
   answers: string[];
+  text: string;
   correct_answer_index: number;
 }
 
 const questions: Question[] = [
-  { answers: ["nikos", "maria", "dimitra"], correct_answer_index: 1 },
-  { answers: ["asdklfjklasd", "akllk", "kat"], correct_answer_index: 0 },
+  {
+    text: "who is the best?",
+    answers: ["nikos", "maria", "dimitra"],
+    correct_answer_index: 1,
+  },
+  {
+    text: "who is the worst?",
+    answers: ["asdklfjklasd", "akllk", "kat"],
+    correct_answer_index: 0,
+  },
 ];
 
 const checkCorrectAnswer = (answer: number, question: Question) => {
-  return question.correct_answer_index == answer;
+  console.log(question.correct_answer_index == answer);
 };
 </script>
 
 <template>
   <div class="flex flex-col gap-8 min-w-dvw min-h-dvh items-center py-5">
     <h1 class="text-6xl">Practical Quiz</h1>
-    <div v-for="question in questions" class="flex gap-3">
-      <Button
-        v-for="(answer, answer_idx) in question.answers"
-        class="w-80 h-40"
-        :label="answer"
-        @click="() => checkCorrectAnswer(answer_idx, question)"
-        :pt="{ label: { class: 'text-3xl' } }"
-      />
+    <div v-for="question in questions">
+      <p class="text-3xl border rounded-2xl p-5 mb-4">{{ question.text }}</p>
+      <div class="flex gap-3">
+        <Button
+          v-for="(answer, answer_idx) in question.answers"
+          class="w-80 h-40 mb-8"
+          :label="answer"
+          @click="() => checkCorrectAnswer(answer_idx, question)"
+          :pt="{ label: { class: 'text-3xl' } }"
+        />
+      </div>
     </div>
   </div>
 </template>
