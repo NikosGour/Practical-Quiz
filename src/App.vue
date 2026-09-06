04dadaf11761bf1b910fb9ad10baddde91b71cac
NikosGour <nikosgournak@gmail.com>
2026-09-06 19:56:42 +0300

    fix sizes

--- src/App.vue
+++ src/App.vue
@@ -52,37 +52,35 @@ const checkCorrectAnswer = (
 <template>
   <div class="flex flex-col gap-8 min-w-dvw min-h-dvh items-center py-5">
     <h1 class="text-6xl">Practical Quiz</h1>
-    <div v-for="test in tests" :key="test.id">
-      <div class="max-w-[80%]">
-        <div v-for="question in test.questions" :key="question.id">
-          <p class="text-2xl border rounded-2xl p-5 mb-4">
-            {{ question.text }}
-          </p>
-          <div class="flex flex-wrap md:flex-nowrap justify-around gap-4 mb-16">
-            <Button
-              v-for="(answer, answer_idx) in question.answers"
-              :key="answer_idx"
-              class="w-80 h-40"
-              :label="answer"
-              @click="() => checkCorrectAnswer(answer_idx, question, test.id)"
-              :pt="{
-                label: { class: 'text-white text-2xl' },
-                // root: { class: 'bg-stone-600! border-stone-400!' },
-                root: {
-                  class:
-                    question.chosen_anwser_index === undefined
-                      ? 'bg-stone-600! border-stone-400!'
-                      : answer_idx === question.correct_answer_index
-                        ? 'bg-green-400! border-green-300!'
-                        : answer_idx === question.chosen_anwser_index &&
-                            question.chosen_anwser_index !==
-                              question.correct_answer_index
-                          ? 'bg-red-400! border-red-300!'
-                          : 'bg-stone-600! border-stone-400!',
-                },
-              }"
-            />
-          </div>
+    <div v-for="test in tests" :key="test.id" class="max-w-[80%]">
+      <div v-for="question in test.questions" :key="question.id">
+        <p class="text-2xl border rounded-2xl p-5 mb-4">
+          {{ question.text }}
+        </p>
+        <div class="flex flex-wrap md:flex-nowrap justify-center gap-4 mb-16">
+          <Button
+            v-for="(answer, answer_idx) in question.answers"
+            :key="answer_idx"
+            class="w-80 h-40"
+            :label="answer"
+            @click="() => checkCorrectAnswer(answer_idx, question, test.id)"
+            :pt="{
+              label: { class: 'text-white text-2xl' },
+              // root: { class: 'bg-stone-600! border-stone-400!' },
+              root: {
+                class:
+                  question.chosen_anwser_index === undefined
+                    ? 'bg-stone-600! border-stone-400!'
+                    : answer_idx === question.correct_answer_index
+                      ? 'bg-green-400! border-green-300!'
+                      : answer_idx === question.chosen_anwser_index &&
+                          question.chosen_anwser_index !==
+                            question.correct_answer_index
+                        ? 'bg-red-400! border-red-300!'
+                        : 'bg-stone-600! border-stone-400!',
+              },
+            }"
+          />
         </div>
       </div>
     </div>
